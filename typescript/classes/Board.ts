ad3ec7668e2c4779d4afe4df5891d4441c7a4cad
Eric Classon <142800136+EricClasson@users.noreply.github.com>
2024-08-26 13:36:25 +0200

    fixing board render

--- typescript/classes/Board.ts
+++ typescript/classes/Board.ts
@@ -2,27 +2,20 @@ export default class Board {
   matrix: string[][];
 
   constructor() {
-    this.matrix = [
-      ["", "O", "X", "O", "X", "O", "X"],
-      ["O", "X", "O", "X", "O", "X", "O"],
-      ["X", "X", "O", "X", "O", "X", "O"],
-      ["O", "O", "X", "O", "X", "O", "X"],
-      ["X", "O", "X", "X", "O", "X", "O"],
-      ["O", "X", "O", "X", "O", "X", "O"],
-    ];
-
-    // this.matrix = [...new Array(6)].map(() => [...new Array(7)].map(() => ""));
+    this.matrix = [...new Array(6)].map(() => [...new Array(7)].map(() => ""));
   }
 
   render() {
     const columnNumber =
-      "  " + [...Array(7).keys()].map((n) => n + 1).join("  ") + "";
-    let line = "\n" + "-".repeat(22) + "\n";
+      "  " + [...Array(7).keys()].map((n) => n + 1).join("   ") + "";
+    let line = "\n" + "-".repeat(29) + "\n";
     console.log(
       columnNumber +
         line +
         this.matrix
-          .map((row) => row.map((marker) => `| ${marker} `).join("") + "|")
+          .map(
+            (row) => row.map((marker) => `| ${marker || " "} `).join("") + "|"
+          )
           .join(line) +
         line
     );
